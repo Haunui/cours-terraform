97e634101c0f5ca0bc942da295033a031f13dadb
Haunui Saint-sevin <haunui@saint-sevin.fr>
2022-01-05 17:53:11 +0100

    Re: Change variables syntax

--- datasources.tf
+++ datasources.tf
@@ -1,8 +1,9 @@
 # CLIENT CONFIG FOR KEY VAULT
 data "azurerm_client_config" "current" {}
 
+
 # RAPHAEL'S STORAGE
 data "azurerm_storage_account" "straphael" {
-    name = "${var.data["raphael"]["name"]}"
-    resource_group_name = "${var.data["raphael"]["resource_group_name"]}"
+  name = "${var.data.raphael.name}"
+  resource_group_name = "${var.data.raphael.resource_group_name}"
 }
